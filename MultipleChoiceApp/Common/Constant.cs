3ca488ee4c52569e9ea585bae4547d6c6aeb232c
Peter Anh <webfullstack99@gmail.com>
2021-11-29 20:22:12 +0700

    create sidebar

---
+++ MultipleChoiceApp/Common/Constant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceApp.Common
+{
+    class Constant
+    {
+        public String primaryColor = "#2196f3";
+        public String secondaryColor = "#f50057";
+        public String thridColor = "#AB987A";
+        public String neutralColor = "#FSFSFS";
+    }
+}
