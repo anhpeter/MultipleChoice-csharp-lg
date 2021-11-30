0093465bdc5ef257c899b4bf83a941467f542b16
Peter Anh <webfullstack99@gmail.com>
2021-11-30 13:26:36 +0700

    .

--- MultipleChoiceApp/Forms/FrmExamStart.Designer.cs
+++ MultipleChoiceApp/Forms/FrmExamStart.Designer.cs
@@ -46,6 +46,9 @@ namespace MultipleChoiceApp.Forms
             this.lbl_class = new System.Windows.Forms.Label();
             this.lbl_duration = new System.Windows.Forms.Label();
             this.lbl_question_qty = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.bunifuFlatButton2 = new Bunifu.Framework.UI.BunifuFlatButton();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -53,12 +56,14 @@ namespace MultipleChoiceApp.Forms
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.tableLayoutPanel1);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(53, 33);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(53, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(956, 344);
+            this.panel1.Size = new System.Drawing.Size(956, 311);
             this.panel1.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -81,18 +86,18 @@ namespace MultipleChoiceApp.Forms
             this.tableLayoutPanel1.Controls.Add(this.lbl_class, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.lbl_duration, 3, 1);
             this.tableLayoutPanel1.Controls.Add(this.lbl_question_qty, 3, 2);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(247, 58);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(248, 83);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(655, 134);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(655, 165);
             this.tableLayoutPanel1.TabIndex = 7;
             // 
             // drop_level
             // 
-            this.drop_level.BackColor = System.Drawing.SystemColors.Control;
+            this.drop_level.BackColor = System.Drawing.Color.White;
             this.drop_level.BorderRadius = 1;
             this.drop_level.Color = System.Drawing.Color.Black;
             this.drop_level.Direction = Bunifu.UI.WinForms.BunifuDropdown.Directions.Down;
@@ -126,7 +131,7 @@ namespace MultipleChoiceApp.Forms
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(3, 86);
+            this.label7.Location = new System.Drawing.Point(3, 116);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(46, 19);
             this.label7.TabIndex = 9;
@@ -148,7 +153,7 @@ namespace MultipleChoiceApp.Forms
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(3, 43);
+            this.label3.Location = new System.Drawing.Point(3, 58);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(93, 19);
             this.label3.TabIndex = 5;
@@ -170,7 +175,7 @@ namespace MultipleChoiceApp.Forms
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(355, 43);
+            this.label5.Location = new System.Drawing.Point(355, 58);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 19);
             this.label5.TabIndex = 7;
@@ -181,7 +186,7 @@ namespace MultipleChoiceApp.Forms
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(355, 86);
+            this.label6.Location = new System.Drawing.Point(355, 116);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(98, 19);
             this.label6.TabIndex = 8;
@@ -190,9 +195,9 @@ namespace MultipleChoiceApp.Forms
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(36, 44);
+            this.pictureBox1.Location = new System.Drawing.Point(71, 80);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(181, 215);
+            this.pictureBox1.Size = new System.Drawing.Size(136, 165);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -200,11 +205,12 @@ namespace MultipleChoiceApp.Forms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Gray;
-            this.label1.Location = new System.Drawing.Point(72, 17);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(381, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 28);
+            this.label1.Size = new System.Drawing.Size(197, 28);
             this.label1.TabIndex = 3;
             this.label1.Text = "Student Information";
             // 
@@ -224,7 +230,7 @@ namespace MultipleChoiceApp.Forms
             this.lbl_dob.AutoSize = true;
             this.lbl_dob.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_dob.ForeColor = System.Drawing.Color.Black;
-            this.lbl_dob.Location = new System.Drawing.Point(108, 43);
+            this.lbl_dob.Location = new System.Drawing.Point(108, 58);
             this.lbl_dob.Name = "lbl_dob";
             this.lbl_dob.Size = new System.Drawing.Size(56, 19);
             this.lbl_dob.TabIndex = 10;
@@ -235,7 +241,7 @@ namespace MultipleChoiceApp.Forms
             this.lbl_class.AutoSize = true;
             this.lbl_class.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_class.ForeColor = System.Drawing.Color.Black;
-            this.lbl_class.Location = new System.Drawing.Point(108, 86);
+            this.lbl_class.Location = new System.Drawing.Point(108, 116);
             this.lbl_class.Name = "lbl_class";
             this.lbl_class.Size = new System.Drawing.Size(60, 19);
             this.lbl_class.TabIndex = 10;
@@ -246,7 +252,7 @@ namespace MultipleChoiceApp.Forms
             this.lbl_duration.AutoSize = true;
             this.lbl_duration.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_duration.ForeColor = System.Drawing.Color.Black;
-            this.lbl_duration.Location = new System.Drawing.Point(467, 43);
+            this.lbl_duration.Location = new System.Drawing.Point(467, 58);
             this.lbl_duration.Name = "lbl_duration";
             this.lbl_duration.Size = new System.Drawing.Size(86, 19);
             this.lbl_duration.TabIndex = 10;
@@ -257,18 +263,104 @@ namespace MultipleChoiceApp.Forms
             this.lbl_question_qty.AutoSize = true;
             this.lbl_question_qty.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_question_qty.ForeColor = System.Drawing.Color.Black;
-            this.lbl_question_qty.Location = new System.Drawing.Point(467, 86);
+            this.lbl_question_qty.Location = new System.Drawing.Point(467, 116);
             this.lbl_question_qty.Name = "lbl_question_qty";
             this.lbl_question_qty.Size = new System.Drawing.Size(112, 19);
             this.lbl_question_qty.TabIndex = 10;
             this.lbl_question_qty.Text = "<question qty>";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label8.Location = new System.Drawing.Point(48, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 28);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Exam:";
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Active = false;
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton1.BorderRadius = 0;
+            this.bunifuFlatButton1.ButtonText = "Start";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 90D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(275, 453);
+            this.bunifuFlatButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(250, 60);
+            this.bunifuFlatButton1.TabIndex = 5;
+            this.bunifuFlatButton1.Text = "Start";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // bunifuFlatButton2
+            // 
+            this.bunifuFlatButton2.Active = false;
+            this.bunifuFlatButton2.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.bunifuFlatButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.bunifuFlatButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton2.BorderRadius = 0;
+            this.bunifuFlatButton2.ButtonText = "Exit";
+            this.bunifuFlatButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton2.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton2.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton2.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton2.Iconimage")));
+            this.bunifuFlatButton2.Iconimage_right = null;
+            this.bunifuFlatButton2.Iconimage_right_Selected = null;
+            this.bunifuFlatButton2.Iconimage_Selected = null;
+            this.bunifuFlatButton2.IconMarginLeft = 0;
+            this.bunifuFlatButton2.IconMarginRight = 0;
+            this.bunifuFlatButton2.IconRightVisible = true;
+            this.bunifuFlatButton2.IconRightZoom = 0D;
+            this.bunifuFlatButton2.IconVisible = true;
+            this.bunifuFlatButton2.IconZoom = 90D;
+            this.bunifuFlatButton2.IsTab = false;
+            this.bunifuFlatButton2.Location = new System.Drawing.Point(615, 453);
+            this.bunifuFlatButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuFlatButton2.Name = "bunifuFlatButton2";
+            this.bunifuFlatButton2.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.bunifuFlatButton2.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.bunifuFlatButton2.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton2.selected = false;
+            this.bunifuFlatButton2.Size = new System.Drawing.Size(250, 60);
+            this.bunifuFlatButton2.TabIndex = 6;
+            this.bunifuFlatButton2.Text = "Exit";
+            this.bunifuFlatButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuFlatButton2.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton2.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
             // FrmExamStart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(1071, 556);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bunifuFlatButton2);
+            this.Controls.Add(this.bunifuFlatButton1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -276,6 +368,7 @@ namespace MultipleChoiceApp.Forms
             this.Name = "FrmExamStart";
             this.Text = "FrmExamStart";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -302,5 +395,8 @@ namespace MultipleChoiceApp.Forms
         private System.Windows.Forms.Label lbl_class;
         private System.Windows.Forms.Label lbl_duration;
         private System.Windows.Forms.Label lbl_question_qty;
+        private System.Windows.Forms.Label label8;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton2;
     }
 }
