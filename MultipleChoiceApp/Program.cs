6f0ca44dce409023899259857e53299bfd511f3c
Peter Anh <webfullstack99@gmail.com>
2021-11-30 12:50:04 +0700

    add login

--- MultipleChoiceApp/Program.cs
+++ MultipleChoiceApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultipleChoiceApp.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,8 @@ namespace MultipleChoiceApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmAdmin());
+            //Application.Run(new FrmAdmin());
+            Application.Run(new FrmLogin());
         }
     }
 }
