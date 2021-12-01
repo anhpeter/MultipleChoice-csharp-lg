f05844bca3e0d7758ce6289620c9436e2bea44b7
Peter Anh <webfullstack99@gmail.com>
2021-12-01 11:09:36 +0700

    add db helper

--- MultipleChoiceApp/Common/Helpers/DBHelper.cs
+++ MultipleChoiceApp/Common/Helpers/DBHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,71 @@ namespace MultipleChoiceApp.Common.Helpers
 {
     abstract class DBHelper
     {
+        String conStr = ConfigurationManager.ConnectionStrings["strCon"].ConnectionString;
+        SqlConnection con;
+
+        private SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection(conStr);
+            return con;
+        }
+
+        public SqlDataReader read(String sql)
+        {
+            try
+            {
+                con = getConnection();
+                con.Open();
+                SqlCommand com = new SqlCommand(sql, con);
+                SqlDataReader dr = com.ExecuteReader();
+                con.Close();
+                return dr;
+                //
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public int execWrite(String sql)
+        {
+            try
+            {
+                con = getConnection();
+                con.Open();
+                SqlCommand com = new SqlCommand(sql, con);
+                int result = com.ExecuteNonQuery();
+                con.Close();
+                return result;
+                //
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        public bool closeConnection()
+        {
+            if (con != null)
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    try
+                    {
+                        con.Close();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
